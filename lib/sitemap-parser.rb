b1bea7e4c1cf92b175d2d28089366eebbe981bc5
Scott Johnson <fuzzygroup@gmail.com>
2017-11-27 08:18:37 -0500

    Add a user agent to get past a mild level of detection

--- lib/sitemap-parser.rb
+++ lib/sitemap-parser.rb
@@ -12,6 +12,8 @@ class SitemapParser
     @raw_sitemap ||= begin
       if @url =~ /\Ahttp/i
         request_options = @options.dup.tap { |opts| opts.delete(:recurse); opts.delete(:url_regex) }
+        # jsj - force in a user agent to get past udemy blocks
+        request_options = {'User-Agent' => "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"} #if request_options['User-Agent'].nil?
         request = Typhoeus::Request.new(@url, request_options)
         request.on_complete do |response|
           if response.success?
