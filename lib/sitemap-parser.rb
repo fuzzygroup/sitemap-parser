44dc63c75823124404adb10092d78d02c7876f59
Scott Johnson <fuzzygroup@gmail.com>
2017-11-27 09:04:41 -0500

    Another attempt

--- lib/sitemap-parser.rb
+++ lib/sitemap-parser.rb
@@ -17,6 +17,7 @@ class SitemapParser
         #request_options = {:agent => "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"} #if request_options['User-Agent'].nil?
         #https://github.com/typhoeus/typhoeus/issues/78
         #request_options = {:user_agent => "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"} #if request_options['User-Agent'].nil?
+        request_options["User-Agent"] = "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"
         request = Typhoeus::Request.new(@url, request_options)
         debugger
         #,  :headers=>{"User-Agent" => "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"})
