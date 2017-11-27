6d73497b5323ee5e126bbdb72366463b8ab10cbb
Scott Johnson <fuzzygroup@gmail.com>
2017-11-27 08:43:07 -0500

    yet another attempt to inject a user_agent header

--- lib/sitemap-parser.rb
+++ lib/sitemap-parser.rb
@@ -17,8 +17,9 @@ class SitemapParser
         #request_options = {:agent => "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"} #if request_options['User-Agent'].nil?
         #https://github.com/typhoeus/typhoeus/issues/78
         #request_options = {:user_agent => "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"} #if request_options['User-Agent'].nil?
-        #debugger
-        request = Typhoeus::Request.new(@url, request_options,  :headers=>{"User-Agent" => "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"})
+        debugger
+        request = Typhoeus::Request.new(@url, request_options)
+        #,  :headers=>{"User-Agent" => "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"})
         request.on_complete do |response|
           if response.success?
             return response.body
